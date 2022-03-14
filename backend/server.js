bdb74835ec9112f08c55e5517e8d5b396ab32452
vijay0707 <vijaysaran0707@gmail.com>
2022-03-14 20:49:13 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware')
+const {
+  errorHandler
+} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
@@ -10,7 +13,9 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({
+  extended: false
+}))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
